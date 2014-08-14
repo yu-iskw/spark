d0cdba49a445e3b94ba39ef1d12f697368b57332
Yuu ISHIKAWA <yuu.ishikawa@gmail.com>
2014-08-14 18:44:58 +0900

    Add CosineDistanceMeasure

---
+++ mllib/src/main/scala/org/apache/spark/mllib/linalg/distance/CosineDistanceMeasure.scala
@@ -0,0 +1,35 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.mllib.linalg.distance
+
+import org.apache.spark.mllib.linalg
+
+class CosineDistanceMeasure extends DistanceMeasure {
+  /**
+   * Calculates the cosine distance between 2 points
+   *
+   * @param v1 a Vector defining a multidimensional point in some feature space
+   * @param v2 a Vector defining a multidimensional point in some feature space
+   * @return a scalar doubles of the distance
+   */
+  override def distance(v1: linalg.Vector, v2: linalg.Vector): Double = {
+    val dotProduct = v1.toBreeze.dot(v2.toBreeze)
+    val denominator = v1.toBreeze.norm(2) * v2.toBreeze.norm(2)
+    1 - (dotProduct / denominator)
+  }
+}
