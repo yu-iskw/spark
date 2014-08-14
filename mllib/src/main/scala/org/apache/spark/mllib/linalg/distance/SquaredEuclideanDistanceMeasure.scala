80d8fb7f56893d5089338702f184fc29591e20ae
Yuu ISHIKAWA <yuu.ishikawa@gmail.com>
2014-08-15 07:45:47 +0900

    Format code which has fault in terms of scalastyle

--- mllib/src/main/scala/org/apache/spark/mllib/linalg/distance/SquaredEuclideanDistanceMeasure.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/linalg/distance/SquaredEuclideanDistanceMeasure.scala
@@ -6,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -29,6 +29,8 @@ class SquaredEuclideanDistanceMeasure extends DistanceMeasure {
    * @return a scalar doubles of the distance
    */
   override def distance(v1: linalg.Vector, v2: linalg.Vector): Double = {
-    (v1.toArray zip v2.toArray).map { case (elm1: Double, elm2: Double) => Math.pow(elm1 - elm2, 2)}.sum
+    (v1.toArray zip v2.toArray).map {
+      case (elm1: Double, elm2: Double) => Math.pow(elm1 - elm2, 2)
+    }.sum
   }
 }
