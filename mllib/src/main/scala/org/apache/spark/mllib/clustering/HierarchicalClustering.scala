6a0074b9f025d7e918100701167e2f89daae2714
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2015-04-02 14:24:58 +0900

    Change the parameters of HierarchicalClustering from public to private

--- mllib/src/main/scala/org/apache/spark/mllib/clustering/HierarchicalClustering.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/clustering/HierarchicalClustering.scala
@@ -98,7 +98,7 @@ object HierarchicalClustering extends Logging {
  * @param maxRetries the number of maximum retries
  * @param seed a random seed
  */
-class HierarchicalClustering(
+class HierarchicalClustering private (
   private var numClusters: Int,
   private var clusterMap: Map[Long, ClusterTree],
   private var maxIterations: Int,
