ec03928183eafc9d932366afa821c90f5adec23c
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2015-04-23 20:26:04 +0900

    Create the basic wrapper classes for HierarchicalClustering

---
+++ mllib/src/main/scala/org/apache/spark/ml/clustering/HierarchicalClustering.scala
@@ -0,0 +1,161 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.spark.ml.clustering
+
+import org.apache.spark.annotation.AlphaComponent
+import org.apache.spark.ml.param._
+import org.apache.spark.ml.{Estimator, Model}
+import org.apache.spark.mllib
+import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{DataFrame, Row}
+
+/**
+ * :: AlphaComponent ::
+ *
+ * TODO write a description for the hierarchical clustering parameters.
+ */
+@AlphaComponent
+private[clustering]
+trait HierarchicalClusteringParams extends Params with HasMaxIter with HasInputCol {
+
+  /**
+   * Param for the number of clusters you want.
+   * @group param
+   */
+  val numClusters = new IntParam(this, "numClusters", "number of clusters you want")
+
+  def getNumClusters(): Int = get(numClusters)
+
+  /**
+   * Param for the maximum number of retries
+   * @group param
+   */
+  val maxRetries = new IntParam(this, "maxRetries", "maximum number of retries")
+
+  def getMaxRetries(): Int = get(maxRetries)
+
+  /**
+   * Param for a random seed
+   * @group param
+   */
+  val seed = new IntParam(this, "seed", "random seed")
+
+  def getSeed(): Int = get(seed)
+
+  /**
+   * Validates and transforms the input schema.
+   * @param schema input schema
+   * @param paramMap extra params
+   * @return output schema
+   */
+  protected def validateAndTransformSchema(schema: StructType, paramMap: ParamMap): StructType = {
+    val map = this.paramMap ++ paramMap
+    assert(schema(map(inputCol)).dataType == ArrayType)
+    schema
+  }
+}
+
+/**
+ * :: AlphaComponent ::
+ *
+ * TODO write a description for the hierarchical clustering.
+ */
+@AlphaComponent
+class HierarchicalClustering
+    extends Estimator[HierarchicalClusteringModel] with HierarchicalClusteringParams {
+
+  setMaxIter(20)
+  setMaxRetries(5)
+  setSeed(1)
+
+  /** @group setParam */
+  def setNumClusters(value: Int): this.type = set(numClusters, value)
+
+  /** @group setParam */
+  def setMaxIter(value: Int): this.type = set(maxIter, value)
+
+  /** @group setParam */
+  def setMaxRetries(value: Int): this.type = set(maxRetries, value)
+
+  /** @group setParam */
+  def setSeed(value: Int): this.type = set(seed, value)
+
+  def setInputCol(value: String): this.type = set(inputCol, value)
+
+  /**
+   * Fits a single model to the input data with provided parameter map.
+   *
+   * @param dataset input dataset
+   * @param paramMap Parameter map.
+   *                 These values override any specified in this Estimator's embedded ParamMap.
+   * @return fitted model
+   */
+  override def fit(dataset: DataFrame, paramMap: ParamMap): HierarchicalClusteringModel = {
+    val map = this.paramMap ++ paramMap
+    val oldData = extractVector(dataset, paramMap)
+    val algo = new mllib.clustering.HierarchicalClustering()
+        .setNumClusters(map(numClusters))
+        .setMaxIterations(map(maxIter))
+        .setMaxRetries(map(maxRetries))
+        .setSeed(map(seed))
+    val parentModel = algo.run(oldData)
+    val model = new HierarchicalClusteringModel(this, map, parentModel)
+    model
+  }
+
+  def extractVector(dataset: DataFrame, paramMap: ParamMap): RDD[Vector] = {
+    val map = this.paramMap ++ paramMap
+    dataset.select(col(map(inputCol))).map { case Row(point: Vector) => point }
+  }
+
+  /**
+   * :: DeveloperApi ::
+   *
+   * Derives the output schema from the input schema and parameters.
+   * The schema describes the columns and types of the data.
+   *
+   * @param schema  Input schema to this stage
+   * @param paramMap  Parameters passed to this stage
+   * @return  Output schema from this stage
+   */
+  override def transformSchema(schema: StructType, paramMap: ParamMap): StructType = schema
+}
+
+
+/**
+ * :: AlphaComponent ::
+ *
+ * Model produced by [[HierarchicalClustering]].
+ */
+@AlphaComponent
+class HierarchicalClusteringModel private[ml](
+    override val parent: HierarchicalClustering,
+    override val fittingParamMap: ParamMap,
+    val parentModel: mllib.clustering.HierarchicalClusteringModel)
+    extends Model[HierarchicalClusteringModel] with HierarchicalClusteringParams {
+
+  def predict(point: Vector): Int = {
+    parentModel.predict(point)
+  }
+
+  override def transform(dataset: DataFrame, paramMap: ParamMap): DataFrame = dataset
+
+  override def transformSchema(schema: StructType, paramMap: ParamMap): StructType = schema
+}
---
+++ mllib/src/test/scala/org/apache/spark/ml/clustering/HierarchicalClusteringSuite.scala
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.spark.ml.clustering
+
+import org.apache.spark.Logging
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.mllib.util.MLlibTestSparkContext
+import org.apache.spark.sql.SQLContext
+import org.scalatest.FunSuite
+
+case class TestDataFrame(point: Vector)
+
+class HierarchicalClusteringSuite extends FunSuite with MLlibTestSparkContext with Logging {
+
+  private var sqlContext: SQLContext = _
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+    sqlContext = new SQLContext(sc)
+  }
+
+  test("fit & predict") {
+    val sqlContext = this.sqlContext
+    // this is used to implicitly convert an RDD to a DataFrame.
+    import sqlContext.implicits._
+
+    val local = (0 to 99).map { i =>
+      val elm = (i % 5).toDouble
+      val point = Vectors.dense(elm, elm, elm)
+      TestDataFrame(point)
+    }
+    val dataset = sc.makeRDD(local, 2).toDF("point")
+
+    val algo = new HierarchicalClustering()
+        .setNumClusters(5)
+        .setInputCol("point")
+        .setMaxIter(20)
+        .setMaxRetries(5)
+        .setSeed(1)
+    val model = algo.fit(dataset)
+
+    assert(model.predict(local(0).point) === model.predict(local(5).point))
+    assert(model.predict(local(1).point) === model.predict(local(6).point))
+    assert(model.predict(local(2).point) === model.predict(local(7).point))
+    assert(model.predict(local(3).point) === model.predict(local(8).point))
+    assert(model.predict(local(4).point) === model.predict(local(9).point))
+
+    assert(model.predict(local(0).point) !== model.predict(local(1).point))
+    assert(model.predict(local(0).point) !== model.predict(local(2).point))
+    assert(model.predict(local(0).point) !== model.predict(local(3).point))
+    assert(model.predict(local(0).point) !== model.predict(local(4).point))
+  }
+}
